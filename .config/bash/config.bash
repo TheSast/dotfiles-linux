89b8bd8c5660108943bde47c17aa57faee6cf9b8
TheSast <27977196+TheSast@users.noreply.github.com>
2023-07-17 18:52:42 +0000

    feat(shell): add zellij auto-start option and support

--- .config/bash/config.bash
+++ .config/bash/config.bash
@@ -16,19 +16,25 @@ shopt -s histappend
 
 # --- Pass Process ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-# live and die with tmux
-if [ -z "$TMUX"  ]; then
-    neofetch # splashscreen
-    read -r -n 1 -s key
-    clear
-    case "$key" in
-        "q"|"Q"|"")
-            return 0
-    esac
-    if tmux list-clients 2> /dev/null | grep -q main; then
-        tmux new-session -d -s mirror -t first
-        tmux set-hook -t mirror client-attached "set -t mirror 'destroy-unattached on'"
-        tmux attach -f 'ignore-size,active-pane' -t mirror && exit
-    fi
-    tmux new-session -A -s main -t first && exit
+# live and die with terminal multiplexers
+if [ -z "$TMUX" ] && [ -z "$ZELLIJ" ]; then
+	neofetch # splashscreen
+	read -r -n 1 -s key
+	clear
+	case "$key" in
+	"q" | "Q" | "")
+		return 0
+		;;
+	"z" | "Z")
+		zellij && exit
+		;;
+	*)
+		# if tmux list-clients 2>/dev/null | grep -q main; then
+		# 	tmux new-session -d -s mirror -t first
+		# 	tmux set-hook -t mirror client-attached "set -st mirror 'destroy-unattached on'"
+		# 	tmux attach -f 'ignore-size,active-pane' -t mirror && exit
+		# fi
+		tmux new-session -A -s main -t first && exit
+		;;
+	esac
 fi
