00241d665e0772d8b39b1ef1add6f337b84106b8
TheSast <27977196+TheSast@users.noreply.github.com>
2023-07-17 18:06:15 +0000

    chore(nvim): remove telescope-media-files.nvim
    telescope-media-files.nvim is broken in tmux, zellij and neovide

--- .config/astronvim/lua/user/plugins/telescope.lua
+++ .config/astronvim/lua/user/plugins/telescope.lua
@@ -1,7 +1,6 @@
 return {
   "telescope.nvim",
   dependencies = {
-    "nvim-telescope/telescope-media-files.nvim",
     "jvgrootveld/telescope-zoxide",
     "debugloop/telescope-undo.nvim",
     opts = function(_, opts)
@@ -13,14 +12,12 @@ return {
     end,
   },
   keys = {
-    { "<leader>fM", "<cmd>Telescope media_files<CR>", desc = "Find media" },
     { "<leader>fz", "<cmd>Telescope zoxide list<CR>", desc = "Find directories" },
     { "<leader>fu", "<cmd>Telescope undo<CR>", desc = "Find undos" },
   },
   config = function(...)
     require "plugins.configs.telescope"(...)
     local telescope = require "telescope"
-    telescope.load_extension "media_files"
     telescope.load_extension "zoxide"
     telescope.load_extension "undo"
   end,
