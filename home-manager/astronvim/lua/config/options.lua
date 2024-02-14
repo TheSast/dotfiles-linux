cf2544b95942a297ad8c2ec607d30f735bc0dc1c
TheSast <27977196+TheSast@users.noreply.github.com>
2024-02-14 16:49:57 +0000

    chore(nvim): cleanup options

--- home-manager/astronvim/lua/config/options.lua
+++ home-manager/astronvim/lua/config/options.lua
@@ -1,39 +1,15 @@
-local background
-if tonumber(os.date "%H") >= 18 or tonumber(os.date "%H") < 06 then
-  background = "dark"
-else
-  background = "light"
-end
-
-vim.g.mapleader = " "
 vim.g.maplocalleader = vim.api.nvim_replace_termcodes("<C-Space>", true, true, true)
 vim.opt.showcmdloc = "statusline"
 vim.opt.title = true
+vim.opt.background = (tonumber(os.date "%H") >= 18 or tonumber(os.date "%H") < 06) and "dark" or "light"
+vim.opt.cpoptions:remove "_"
 vim.opt.titlestring = vim.g.neovide and "Neovide" or "NeoVim"
-vim.opt.guifont = "FiraCode_Nerd_Font_Med:h12"
+vim.opt.guifont = "FiraCode_Nerd_Font_Med:h12" -- TODO: move to neovide toml file
 vim.opt.winblend = 30
 vim.opt.pumblend = 30
--- vim.opt.linespace = -1
-vim.opt.background = background
--- vim.opt.cpoptions = vim.opt.cpoptions:remove "_" --[[or]] -- :gsub("_" "")
-vim.opt.cpoptions = "aABceFs"
 vim.g.neovide_floating_blur_amount_x = 2.0
 vim.g.neovide_floating_blur_amount_y = 2.0
--- vim.g.neovide_transparency = 0.9
-vim.g.neovide_scroll_animation_length = 0.35
 vim.g.neovide_hide_mouse_when_typing = true
 vim.g.neovide_underline_automatic_scaling = true
-vim.g.neovide_padding_top = 0
-vim.g.neovide_padding_bottom = 0
-vim.g.neovide_padding_right = 0
-vim.g.neovide_padding_left = 0
-vim.g.neovide_refresh_rate = 165
 vim.g.neovide_refresh_rate_idle = 5
 vim.g.neovide_no_idle = false
-vim.g.neovide_cursor_animation_length = 0.05
--- vim.g.neovide_cursor_animation_length = 0.13
-vim.g.neovide_cursor_antialiasing = true
-vim.g.neovide_cursor_animate_in_insert_mode = true
-vim.g.neovide_cursor_animate_animate_command_line = false
-vim.g.neovide_cursor_unfocused_outline_width = 0.125
-vim.g.neovide_cursor_vfx_mode = ""
