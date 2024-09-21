e97ccbd450770773c0d715ff00179153ffe15d30
TheSast <27977196+TheSast@users.noreply.github.com>
2024-09-21 12:57:48 +0000

    refactor(nvim): simplify none-ls configuration

--- home-manager/astronvim/lua/plugins/null-ls.lua
+++ home-manager/astronvim/lua/plugins/null-ls.lua
@@ -1,44 +1,59 @@
+--- Filter a list-like table of null-ls sources by available binaries
+---
+--- NOTE: This mutates srcs!
+---@param srcs table # Table of sources to filter
+---@return table avail # The filtered list
+local function handle_nls_avail(srcs)
+  local executable = 1
+  srcs = srcs or {}
+  for i = #srcs, 1, -1 do
+    if vim.fn.executable(srcs[i]._opts.command) ~= executable then table.remove(srcs, i) end
+  end
+  return srcs
+end
+
 return {
   "none-ls.nvim",
   opts = function(_, opts)
-    local nls = require("null-ls").builtins
+    local builtins = require("null-ls").builtins
     -- https://github.com/nvimtools/none-ls.nvim/blob/main/doc/BUILTINS.md
-    local optional_ls = {
-      nls.diagnostics.fish,
-      nls.diagnostics.zsh,
-      nls.diagnostics.alex,
-      nls.diagnostics.stylelint,
-      nls.diagnostics.markdownlint,
+    opts.sources = require("astrocore").extend_tbl(
+      opts.sources,
+      vim.list_extend(
+        handle_nls_avail {
+          builtins.diagnostics.fish,
+          builtins.diagnostics.zsh,
+          builtins.diagnostics.alex,
+          builtins.diagnostics.stylelint,
+          builtins.diagnostics.markdownlint,
 
-      nls.formatting.cmake_format,
-      nls.formatting.leptosfmt,
-      nls.formatting.nixfmt,
-      nls.formatting.yamlfix,
-      nls.formatting.yamlfmt,
-      nls.formatting.shellharden,
-      nls.formatting.shfmt,
-      nls.formatting.fish_indent,
-      nls.formatting.cbfmt,
-      nls.formatting.prettierd,
-    }
-    local permanent_ls = vim.list_extend(opts.servers or {}, {
-      nls.diagnostics.deadnix,
-      nls.diagnostics.statix,
-      nls.diagnostics.selene,
-      -- nls.diagnostics.commitlint, -- NOTE: broken?
-      nls.formatting.alejandra.with {
-        condition = function(_)
-          local executable = 1
-          return vim.fn.executable(nls.formatting.nixfmt._opts.command) ~= executable
-        end,
-      },
-      nls.formatting.stylua,
-      -- nls.hover.printenv,
-      -- nls.hover.dictionary,
-    })
-    for i = #optional_ls, 1, -1 do
-      if vim.fn.executable(optional_ls[i]._opts.command) ~= 1 then table.remove(optional_ls, i) end
-    end
-    opts.sources = vim.list_extend(permanent_ls, optional_ls)
+          builtins.formatting.cmake_format,
+          builtins.formatting.leptosfmt,
+          builtins.formatting.nixfmt,
+          builtins.formatting.yamlfix,
+          builtins.formatting.yamlfmt,
+          builtins.formatting.shellharden,
+          builtins.formatting.shfmt,
+          builtins.formatting.fish_indent,
+          builtins.formatting.cbfmt,
+          builtins.formatting.prettierd,
+        },
+        {
+          builtins.diagnostics.deadnix,
+          builtins.diagnostics.statix,
+          builtins.diagnostics.selene,
+          -- builtins.diagnostics.commitlint, -- NOTE: broken?
+          builtins.formatting.alejandra.with {
+            condition = function(_)
+              local executable = 1
+              return vim.fn.executable(builtins.formatting.nixfmt._opts.command) ~= executable
+            end,
+          },
+          builtins.formatting.stylua,
+          -- nls.hover.printenv,
+          -- nls.hover.dictionary,
+        }
+      )
+    )
   end,
 }
