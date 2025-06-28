6e60759904972a2294a3cffd7109eb2c07895bf2
TheSast <27977196+TheSast@users.noreply.github.com>
2025-06-28 13:48:13 +0000

    fix: broken power button after sleep

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -53,7 +53,20 @@
   systemd.sleep.extraConfig = ''
     HibernateDelaySec=10m
     HibernateMode=shutdown
-  '';
+  ''; # systemd-suspend-then-hibernate delay to hibernation
+  systemd.services.fix-button-after-hibernate = {
+    # it is needed after terminating (by crashing?) user session and redoing login
+    description = "Reload ACPI Button Module After Hibernate";
+    after = ["hibernate.target" "hybrid-sleep.target" "suspend-then-hibernate.target"];
+    wantedBy = ["hibernate.target" "hybrid-sleep.target" "suspend-then-hibernate.target"];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = [
+        "${pkgs.kmod}/bin/modprobe -r button"
+        "${pkgs.kmod}/bin/modprobe button"
+      ];
+    };
+  };
 
   ## Necessary
   networking = {
