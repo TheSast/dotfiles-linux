a8106536ba57c38fd404345328e33428b4ce75cb
TheSast <27977196+TheSast@users.noreply.github.com>
2025-08-01 19:11:20 +0000

    feat(nvim): add time-based colorscheme toggle and help page navigation keymap

--- home-manager/astronvim/lua/plugins/autocmds.lua
+++ home-manager/astronvim/lua/plugins/autocmds.lua
@@ -15,7 +15,23 @@ return {
           end,
         },
       },
+      easy_help = {
+        {
+          event = "FileType",
+          pattern = "help",
+          callback = function() vim.api.nvim_buf_set_keymap(0, "n", "<CR>", "<C-]>", { noremap = true }) end,
+        },
+      },
       -- TODO: add auto opt.background! possibly using https://github.com/f-person/auto-dark-mode.nvim and https://askubuntu.com/questions/22313/what-is-dconf-what-is-its-function-and-how-do-i-use-it
+      time_based_colorscheme = {
+        {
+          event = "FocusGained",
+          callback = function()
+            local correct_bg = (tonumber(os.date "%H") >= 18 or tonumber(os.date "%H") < 06) and "dark" or "light"
+            if correct_bg ~= vim.opt.background:get() then require("astrocore.toggles").background() end
+          end,
+        },
+      },
     },
   },
 }
