0312c7a825e583db856ff797fc12413e0c541741
TheSast <27977196+TheSast@users.noreply.github.com>
2025-08-02 16:19:45 +0000

    fix(nvim): suppress rust-analyzer -32802 diagnostic errors

--- home-manager/astronvim/lua/plugins/lsp/rust.lua
+++ home-manager/astronvim/lua/plugins/lsp/rust.lua
@@ -1,6 +1,18 @@
 ---@type LazySpec
 return {
   {
+    "nvim-lspconfig",
+    init = function(_)
+      for _, method in ipairs { "textDocument/diagnostic", "workspace/diagnostic" } do
+        local default_diagnostic_handler = vim.lsp.handlers[method]
+        vim.lsp.handlers[method] = function(err, result, context, config)
+          if err ~= nil and err.code == -32802 then return end
+          return default_diagnostic_handler(err, result, context, config)
+        end
+      end
+    end,
+  },
+  {
     "astrolsp",
     ---@type AstroLSPOpts
     opts = {
