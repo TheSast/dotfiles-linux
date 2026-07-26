f89d0fd73065dc429283e607ca4334635af24771
TheSast <27977196+TheSast@users.noreply.github.com>
2026-07-26 14:03:53 +0000

    feat(os): use services.displayManager.sessionPackages sessions

--- os/dm.nix
+++ os/dm.nix
@@ -1,22 +1,31 @@
 {
+  config,
   pkgs,
   lib,
   ...
-}: {
+}: let
+  mkSessionArg = flag: dir: let
+    sessions = builtins.concatStringsSep ":" (
+      map (p: "${p}/${dir}") (
+        lib.filter (p: builtins.pathExists "${p}/${dir}") config.services.displayManager.sessionPackages
+      )
+    );
+  in
+    lib.optionalString (sessions != "") " ${flag} ${sessions}";
+in {
+  services.displayManager.sessionPackages = with pkgs; [
+    niri
+  ];
   services.greetd = {
     enable = true;
     useTextGreeter = true;
     settings = {
       default_session = {
-        command = let
-          wayland-session-packages = with pkgs; [
-            niri
-          ];
-          sessions = builtins.concatStringsSep ":" (
-            builtins.map (p: "${p}/share/wayland-sessions") wayland-session-packages
-          );
-          # todo, add steam's gamescope session when available
-        in "${lib.getExe pkgs.tuigreet} --remember --remember-user-session --sessions ${sessions}";
+        command =
+          "${lib.getExe pkgs.tuigreet}"
+          + " --remember --remember-user-session"
+          + mkSessionArg "--sessions" "share/wayland-sessions"
+          + mkSessionArg "--xsessions" "share/xsessions";
         user = "greeter";
       };
     };
