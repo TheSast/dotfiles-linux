1704c977b9de9458fa3b867995208c197535d809
TheSast <27977196+TheSast@users.noreply.github.com>
2026-09-16 19:09:09 +0000

    fix(firefly/os): game launcher not starting game

--- os/firefly/default.nix
+++ os/firefly/default.nix
@@ -69,6 +69,7 @@ in {
       "amdgpu"
       "hid_nintendo"
       "hid_playstation"
+      "ntsync"
     ];
   };
   hardware = {
@@ -121,4 +122,7 @@ in {
     hardware.has.amd.gpu = true;
   };
   services.greetd.enable = lib.mkForce false;
+  networking.extraHosts = ''
+    0.0.0.0 overseauspider.yuanshen.com log-upload-os.hoyoverse.com log-upload-os.mihoyo.com dump.gamesafe.qq.com apm-log-upload-os.hoyoverse.com zzz-log-upload-os.hoyoverse.com log-upload.mihoyo.com devlog-upload.mihoyo.com uspider.yuanshen.com sg-public-data-api.hoyoverse.com hkrpg-log-upload-os.hoyoverse.com public-data-api.mihoyo.com prd-lender.cdp.internal.unity3d.com thind-prd-knob.data.ie.unity3d.com thind-gke-usc.prd.data.corp.unity3d.com cdp.cloud.unity3d.com remote-config-proxy-prd.uca.cloud.unity3d.com pc.crashsight.wetest.net
+  '';
 }
